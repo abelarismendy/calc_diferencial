1aa06d6415fd0c7c2d763c045bc99fcd1e4456e5
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-05-10 15:27:01 -0500

    Sumatoria pendiente de arreglar

--- calc.py
+++ calc.py
@@ -6,14 +6,15 @@ incremento = 10
 inicio = 0
 fin = 3
 f = r'$y=x^2+2$'
+delta = r'$\Delta{x}$'
 
 def graficar_funcion():
     t = arange(0.01, 3, 0.01)
     y = (t**2)+2
     fig, ax = plt.subplots()
-    ax.plot(t, y)
+    ax.plot(t, y, label=f)
     texto = plt.annotate(f, xy = (2,6), xytext = (1.2, 7), fontsize=15,
-        arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"))
+        arrowprops=dict(arrowstyle="<-", connectionstyle="arc3,rad=0"))
     plt.grid()
     return ax
 
@@ -23,7 +24,11 @@ while n <= 100:
     i = 1
     x = inicio
     suma = 0
+    sum_txt =r'$\sum_{i=0}^{3}x_{i}$'
     ax = graficar_funcion()
+    red_patch = patches.Patch(edgecolor='red', fill=False,  label=sum_txt)
+    sumatoria_leyenda = ax.legend(handles=[red_patch], loc='upper right', bbox_to_anchor= (0.2,0.89))
+    ax.add_artist(sumatoria_leyenda)
     while i <= n:
         x += fragmento
         f_de_x = ((x**2)+2)
@@ -36,6 +41,7 @@ while n <= 100:
             fill=False
         ))
         i += 1
-    plt.title(f'Función: {f}\nn = {n}, área = {suma}')
+    plt.title(f'Función: {f}, n = {n}, {delta} = {fragmento}\nárea = {suma}')
+    plt.legend()
     plt.show()
     n += incremento
