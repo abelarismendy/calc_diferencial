f4a9f2267844d1cb7627bdab18357633eadac035
Abel Arismendy <arismendyc.design@gmail.com>
2022-05-25 13:48:11 +0000

    ingral titulo

--- calc_v2.py
+++ calc_v2.py
@@ -8,7 +8,7 @@ end = 3*aux
 f = lambda x: math.sin(x**2)
 diff = 1/aux
 dx = diff/rectangles
-title = r'$G(x)=\int_{0}ˆx{}]\! x \,dx$'
+title = r'$G(x)=\int_{0}^{x} sin(t^2) \cdot dt$'
 
 def integral(a, b, i):
     suma = 0
