3505910401113093049714cd79f33f08fdd6ee7d
Abel Arismendy <arismendyc.design@gmail.com>
2022-05-25 13:34:04 +0000

    prueba

--- calc_v2.py
+++ calc_v2.py
@@ -8,6 +8,7 @@ end = 3*aux
 f = lambda x: math.sin(x**2)
 diff = 1/aux
 dx = diff/rectangles
+title = r'$G(x)=[\int_{0}ˆx{}]\! x \,dx$'
 
 def integral(a, b, i):
     suma = 0
@@ -27,12 +28,13 @@ while i < end:
     a=i/aux
     i+=1
     b=i/aux
-    #suma = integral(0, b, i)
-    suma = integral(a, b, i)
+    suma = integral(0, b, i)
+    #suma = integral(a, b, i)
     x.append(a)
     y.append(suma)
 
 #for p in lista: print(p)
 
 plt.plot(x,y)
-plt.savefig('quiz/a.png', dpi = 200)
+plt.title(title)
+plt.savefig('quiz/0.png', dpi = 200)
